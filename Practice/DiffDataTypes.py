059b59586eceac0802e5ca93c35dc643e3e3118c
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-02 17:52:59 +0530

    New program added

---
+++ Practice/DiffDataTypes.py
@@ -0,0 +1,17 @@
+int = 10 #Interger
+float = 10.2 #Float
+bool = True #Boolean
+str = "Hello World" #String
+list = [1,2,3,4,5] #List
+tuple = (1,2,3,4,5) #Tuple
+set = {1,2,3,4,5} #Set
+dict = {"name":"Mayank","age":20} #Dictionary
+
+print("The data type of int is",type(int))
+print("The data type of float is",type(float))
+print("The data type of bool is",type(bool))
+print("The data type of str is",type(str))
+print("The data type of list is",type(list))
+print("The data type of tuple is",type(tuple))
+print("The data type of set is",type(set))
+print("The data type of dict is",type(dict))
