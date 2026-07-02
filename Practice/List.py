dbf84d99e54f9e9d4387d7133d488af06e525b7c
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-02 18:15:23 +0530

    New program added

---
+++ Practice/List.py
@@ -0,0 +1,8 @@
+list = [14,15,16,13,12,11]
+
+print(type(list))
+print("The list is:",list)
+print("The first element of the list is:",list[0])
+print("The last element of the list is:",list[-1])
+print("The elements of the list from index 1 to 4 are:",list[1:5])
+print("The elements of the list from index 0 to 4 are:",list[:4])
