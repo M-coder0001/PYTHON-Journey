b05a0e13ffa79f9357e5e3247efb0a44e37744d3
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-07 17:58:52 +0530

    New program added

---
+++ Practice/ArithmaticOpr.py
@@ -0,0 +1,10 @@
+a = 10
+b = 5
+
+print("Addition of a and b is:", a + b)
+print("Subtraction of a and b is:", a - b)
+print("Multiplication of a and b is:", a * b)
+print("Division of a and b is:", a / b)
+print("Modulus of a and b is:", a % b)
+print("Exponentiation of a and b is:", a ** b)
+print("Floor division of a and b is:", a // b)
