ef7cb195c26927e628fb3d496f963141318ba0b0
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-20 08:37:21 +0530

    New program added

---
+++ Practice/UserInputValidation.py
@@ -0,0 +1,23 @@
+import hashlib
+
+stored_username = "Admin"
+stored_password = "Admin123"
+
+hash_value = hashlib.sha256(stored_username.encode()).hexdigest()
+hash_value2 = hashlib.sha256(stored_password.encode()).hexdigest()
+
+Username = input ("Enter username: ")
+password = input ("Enter password: ")
+
+hash_value3 = hashlib.sha256(Username.encode()).hexdigest()
+hash_value4 = hashlib.sha256(password.encode()).hexdigest()
+
+if stored_username == Username and stored_password == password:
+    print("Username and Password are verified")
+else:
+    print("Username or password is incorrect")
+    
+
+
+
+
