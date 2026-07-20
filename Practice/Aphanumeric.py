d8fe4d37066b4c994803f3620eb66fdd60d037e4
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-20 08:03:00 +0530

    Add Aphanumeric.py for SHA-256 password hashing

---
+++ Practice/Aphanumeric.py
@@ -0,0 +1,12 @@
+import hashlib
+
+#Take a password input from user
+Password = input ("Enter an alphanumeric password: ")
+
+#Generate SHA-256 Hash value
+hash_value = hashlib.sha256(Password.encode()).hexdigest()
+
+#Display result
+print("Original password", Password)
+print("SHA-256 Hash Value")
+print(hash_valu
