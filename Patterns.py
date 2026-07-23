70cf9a6cdfbe15d487066745d5a8abc517b4081c
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-23 21:46:38 +0530

    new programs added

--- Patterns.py
+++ Patterns.py
@@ -1,15 +1,15 @@
-#left numeric triangle
+print("1. left numeric triangle")
 
-# num = 1
-# for i in range(1,5):
-#     for _ in range(i):
-#         print(num,end = " ")
-#         num += 1
-#     print()
+num = 1
+for i in range(1,5):
+    for _ in range(i):
+        print(num,end = " ")
+        num += 1
+    print()
 
-#==================================
+print("==================================")
     
-# #Floyd's pyramid
+print("2. Floyd's pyramid")
 
 for i in range(6):
     for _ in range(5-i):
@@ -20,23 +20,47 @@ for i in range(6):
         print(j+1,end=" ")
         
     print()
-
-#==================================
-
-# # left star triangle
-# for i in range(5):
-#     for _ in range(i):
-#         print("*", end = " ")
-#     print()
     
-#==================================
+print("==================================")
 
-# # right star triangle 
+print("3. inverted right numeric triangle\n") 
 
+n = 5
 
-#==================================
+for i in range(n, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
 
-# star pyramid
+print("==================================")
+
+print("4. left star triangle")
+for i in range(5):
+    for _ in range(i):
+        print("*", end = " ")
+    print()
+    
+print("==================================")
+
+print("5. right star triangle\n") 
+
+n = 5
+
+for i in range(1, n + 1):
+
+    # Print spaces
+    for _ in range(n - i):
+        print(" ", end=" ")
+
+    # Print stars
+    for _ in range(i):
+        print("*", end=" ")
+
+    print()
+
+print("==================================")
+
+print("6. star pyramid")
 for i in range(5):
     for _ in range(4-i):
         print(" ",end = "")
@@ -45,16 +69,41 @@ for i in range(5):
         print("*", end =" ")
         
     print()
-
-#==================================
-
-#inverted centered star pyramid
-
-# for i in range(5):
-#     for _ in range(4, i):
-#         print(" ",end = "")
     
-#     for _ in range(i):
-#         print("*", end =" ")
-        
-#     print()
+print("==================================")
+
+print("7. inverted right star triangle\n")
+
+n = 5
+
+for i in range(n, 0, -1):
+    for _ in range(i):
+        print("*", end=" ")
+    print()
+
+print("==================================")
+
+print("8. inverted star pyramid\n")
+
+n = 4
+
+for i in range(n, 0, -1):
+
+    # Print leading spaces
+    for _ in range(n - i):
+        print(" ", end="")
+
+    # Print stars
+    for _ in range(i):
+        print("*", end=" ")
+
+    print()
+
+print("==================================")
+
+print("Alphabet Triangle\n")
+
+for i in range(5):
+    for j in range(i + 1):
+        print(chr(65 + j), end=" ")
+    print()
