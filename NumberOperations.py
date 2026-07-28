ce7088d1d9d470e629b4a67fc6764a25e6a5328c
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-28 08:39:59 +0530

    new programs added

--- NumberOperations.py
+++ NumberOperations.py
@@ -1,2 +1,26 @@
 n = int(input("Enter total count of numbers: "))
 positives = []
+negatives = []
+evens = []
+odds = []
+total = 0
+
+for i in range(n):
+    num  = int(input("Enter number: "))
+    if num > 0:
+        positives.append(num)
+    elif num < 0:
+        negatives.append(num)
+    if num % 2 == 0:
+        evens.append(num)
+    else:
+        odds.append(num)
+    
+    total += num
+
+print("======Result======")
+print("positive Nums: ", positives)
+print("Negative Nums: ", negatives)
+print("Even Nums: ", evens)
+print("Odd Nums: ", odds)
+print("Average: ", total / n)
