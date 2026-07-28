7d795428c4aeab0db41e46e4b07d331a14aed26b
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-28 08:03:40 +0530

    new programs added

---
+++ ListOperations.py
@@ -0,0 +1,20 @@
+list = [1,6,3,8,2]
+print("The list: ",list)
+
+list.append(5)
+print("After appending: ", list)
+
+list.remove(8)
+print("After removing: ", list)
+
+print(list)
+list.reverse()
+print("Reversed list: ", list)
+
+print("Length: ", len(list))
+
+list.sort()
+print("Sorted list: ", list)
+
+list.reverse()
+print(list)
