9afa2dd86c51d7be96120b1d0da9ed347313ea84
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-28 08:18:33 +0530

    new programs added

---
+++ NumberOperations.py
@@ -0,0 +1,2 @@
+n = int(input("Enter total count of numbers: "))
+positives = []
--- Patterns.py
+++ Patterns.py
@@ -27,7 +27,7 @@ print("3. inverted right numeric triangle\n")
 
 n = 5
 
-for i in range(n, 0, -1):
+for i in range(n, 1):
     for j in range(1, i + 1):
         print(j, end=" ")
     print()
